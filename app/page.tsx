0da130a9e951ec70b91c9c57c2a1f2de3b87873a
LarbiFelix <larbi.felix@gmail.com>
2025-03-04 10:19:15 +0000

    Migrate ESLint to v9 vers2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-
 const Home = () => (
-   <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of Next.js my!</h1>
-)
+  <h1 className="text-3xl text-violet-700 font-black">
+    Welcome to the world of Next.js my!
+  </h1>
+);
 
-export default Home
+export default Home;

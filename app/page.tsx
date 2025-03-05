1ed8024aeaa8fb64aa37503cf011615679dbd66b
LarbiFelix <larbi.felix@gmail.com>
2025-03-05 02:25:16 +0000

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <h1 className="text-3xl text-violet-700 font-black">
+  <h1 className="text-3xl font-black text-light-500">
     Welcome to the world of Next.js my!
   </h1>
 );

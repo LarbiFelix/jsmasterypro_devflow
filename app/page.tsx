3127f7d427d1541a5dd18af58fe568043d7dd69d
LarbiFelix <larbi.felix@gmail.com>
2025-03-05 05:05:30 +0000

    Assets & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-light-500">
-    Welcome to the world of Next.js my!
-  </h1>
+  <>
+    <h1 className="h1-bold">Welcome to the world of Next.js my!</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the world of Next.js my! (INTER)
+    </h1>
+  </>
 );
 
 export default Home;
